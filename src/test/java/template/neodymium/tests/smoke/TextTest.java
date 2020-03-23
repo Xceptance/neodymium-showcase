34985b4687402345872124162f879070d3ebcab6
Marcel Pfotenhauer <m.pfotenhauer@xceptance.net>
2020-03-23 15:47:19 +0100

    fixed test case by adding switch to iframe

--- src/test/java/template/neodymium/tests/smoke/TextTest.java
+++ src/test/java/template/neodymium/tests/smoke/TextTest.java
@@ -1,14 +1,13 @@
 package template.neodymium.tests.smoke;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.switchTo;
+
 import org.junit.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Condition.matchText;
-import static com.codeborne.selenide.Condition.text;
-
-import com.xceptance.neodymium.util.Neodymium;
-import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.SelenideElement;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Owner;
@@ -28,14 +27,21 @@ import template.pageobjects.pages.HomePage;
 @Tag("smoke")
 @DisplayName("TextTest")
 
-public class TextTest extends AbstractTest {
-	
-	@Test
-	@Description(value = "Check that Textfield exists and")
-	public void checkTextExists() {
-		HomePage homePage = OpenHomePageFlow.flow();
-		SelenideElement text = $(Selectors.shadowCss("p", "show-hello[name=John]"));
-		text.exists();
-		text.has(text("Hello, John"));
-	}
+public class TextTest extends AbstractTest
+{
+
+    @Test
+    @Description(value = "Check that Textfield exists and")
+    public void checkTextExists()
+    {
+        HomePage homePage = OpenHomePageFlow.flow();
+
+        switchTo().frame($("iframe[name='code-result-d3mo39']"));
+
+        SelenideElement text = $(Selectors.shadowCss("p", "show-hello[name=John]"));
+        text.exists();
+        text.shouldHave(text("Hello"));
+
+        switchTo().defaultContent();
+    }
 }
