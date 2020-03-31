245f849d4d2db675706289aff56de9e76d5b8f32
georgkunze <35028677+georgkunze@users.noreply.github.com>
2020-03-31 17:44:18 +0200

    Updated Tests
    Updated all tests and the repro to more reflect its purpose as demonstration example

--- src/test/java/showcase/neodymium/tests/shadowdom/CheckboxTest.java
+++ src/test/java/showcase/neodymium/tests/shadowdom/CheckboxTest.java
@@ -3,8 +3,11 @@ package showcase.neodymium.tests.shadowdom;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.not;
+import static com.codeborne.selenide.Condition.visible;
 
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
@@ -16,10 +19,9 @@ import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.junit4.DisplayName;
 import io.qameta.allure.junit4.Tag;
-import showcase.flows.CheckboxPageFlow;
 import showcase.neodymium.tests.AbstractTest;
 
-import showcase.pageobjects.pages.CheckboxPage;
+import showcase.pageobjects.components.Title;
 
 /**
  * @author kunze
@@ -27,36 +29,53 @@ import showcase.pageobjects.pages.CheckboxPage;
 @Severity(SeverityLevel.TRIVIAL)
 @Owner("Georg Kunze")
 @Tag("smoke")
-@DisplayName("CheckboxTest")
-
-
+@DisplayName("Checkboxtest")
 public class CheckboxTest extends AbstractTest {
         
     @Test
-    @Description(value = "Check the checkbox site for correct Structure")
-    public void testCheckboxSite() {
-        
+    @Description(value = "Check the checkbox site for correct structure. With inner shadow DOM.")
+    public void testInnerShadowDOM() {
         //open demo page
-        CheckboxPage page = CheckboxPageFlow.flow();
+        Selenide.open("https://mwc-demos.glitch.me/demos/checkbox.html");
         
         //check if correct site did open
-        page.isExpectedPage();
+        $("demo-header[component=Checkbox]").should(exist);
         
-        //validate Structure
-        page.validateStructure();
+        //check the top bar is visible
+        $(Selectors.shadowCss("div.mdc-top-app-bar__row", "demo-header[component=Checkbox]", "mwc-top-app-bar-fixed")).shouldBe(visible);
+        
+        //check navigation section is visible
+        $(Selectors.shadowCss("#navigation", "demo-header[component=Checkbox]", "mwc-top-app-bar-fixed")).shouldBe(visible);
+        
+        //check navigation section is visible
+        $(Selectors.shadowCss("#actions", "demo-header[component=Checkbox]", "mwc-top-app-bar-fixed")).shouldBe(visible);
+        
+        //check that the content frame exists
+        $("div.demo-group").shouldBe(visible);
+        
+        //check amount of checkboxes
+        $$("mwc-checkbox").shouldHaveSize(6);
+        
+        //check title
+        new Title().validateTitle("checkbox demo");
     }
     
     @Test
-    @Description(value = "Check the checkbox site for correct Structure")
-    public void testFunctionality() { 
-        
+    @Description(value = "Check the checkbox functionality")
+    public void testCheckbox() { 
         //open demo page
-        CheckboxPage page = CheckboxPageFlow.flow();     
+        Selenide.open("https://mwc-demos.glitch.me/demos/checkbox.html");     
         
-        //check if correct site did open
-        page.isExpectedPage();
+        //Get checkbox
+        SelenideElement checkbox = $(Selectors.shadowCss("div.mdc-checkbox", "mwc-checkbox"));
         
-        //validate checkbox works
-        page.validateShadowCheckboxFunction("div.mdc-checkbox", "mwc-checkbox", "mdc-checkbox--selected");
+        //check checkbox has not the selected class
+        checkbox.shouldHave(not(cssClass("mdc-checkbox--selected")));
+        
+        //click checkbox
+        $("mwc-checkbox").click();
+        
+        //check checkbox has selected class
+        checkbox.shouldHave(cssClass("mdc-checkbox--selected"));
     }
 }
--- src/test/java/showcase/neodymium/tests/shadowdom/TextTest.java
+++ src/test/java/showcase/neodymium/tests/shadowdom/TextTest.java
@@ -1,7 +1,9 @@
 package showcase.neodymium.tests.shadowdom;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.switchTo;
 import static com.codeborne.selenide.Condition.visible;
 
@@ -9,7 +11,6 @@ import org.junit.Test;
 
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Owner;
@@ -17,9 +18,8 @@ import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.junit4.DisplayName;
 import io.qameta.allure.junit4.Tag;
-import showcase.flows.ShadowDomPageFlow;
 import showcase.neodymium.tests.AbstractTest;
-import showcase.pageobjects.pages.ShadowDomPage;
+import showcase.pageobjects.components.Title;
 
 /**
  * @author kunze
@@ -27,39 +27,41 @@ import showcase.pageobjects.pages.ShadowDomPage;
 @Severity(SeverityLevel.TRIVIAL)
 @Owner("Georg Kunze")
 @Tag("smoke")
-@DisplayName("TextTest")
-
+@DisplayName("Texttest")
 public class TextTest extends AbstractTest {
-    
-    @Test
-    @Description(value = "Check structure of Page")
-    public void testLandingPage() {
-        
-        // Goto the javadoc page
-        ShadowDomPage shadowdomPage = ShadowDomPageFlow.flow();
-
-        // short validation to check that the correct page was opened
-        shadowdomPage.isExpectedPage();
-        
-        // basic validation
-        shadowdomPage.validateStructure();
-    }
 	
 	@Test
-	@Description(value = "Check that Textfield exists and control text")
-	public void testTextExists() {
-		
-		//Open javadoc page
-	    ShadowDomPage shadowdomPage = ShadowDomPageFlow.flow();
+	@Description(value = "Check that text field exists and control text")
+	public void testTextField() {
+	    //Open javadoc page
+	    Selenide.open("https://javascript.info/shadow-dom");
 	    
-	    // short validation to check that the correct page was opened
-	    shadowdomPage.isExpectedPage();
+	    //check title
+	    new Title().validateTitle("Shadow DOM");
+	    
+	    // Verifies that toolbar is visible
+        $("div.sitetoolbar").shouldBe(visible);
+        
+        // Verifies that content page is visible
+        $("div.page__inner").shouldBe(visible);
+        
+        // Verifies that sidebar is visible
+        $("div.sidebar__inner").shouldBe(visible);
+        
+        // Verifies that footer is visible
+        $("div.page-footer").shouldBe(visible);
+
+        // Asserts there's categories in the sidebar.
+        $$("div.sidebar__section").shouldHave(sizeGreaterThan(0));
 		
 		//switch to iFrame of content
 		switchTo().frame($("iframe[style=\"height:60px\"]"));
 		
-		//check textfield
-		shadowdomPage.validateShadowTextfield("p", "show-hello[name=John]", "Hello, John");
+		//check text field
+		$(Selectors.shadowCss("p", "show-hello[name=John]")).shouldBe(visible);
+		
+		//check text field has text
+		$(Selectors.shadowCss("p", "show-hello[name=John]")).shouldHave(text("Hello, John"));
 
 		//switch back to Mainframe
 		switchTo().defaultContent();
