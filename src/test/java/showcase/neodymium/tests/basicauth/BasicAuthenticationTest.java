bf7d924d0c732e3af7ab60dc0329b46654e5d995
georgkunze <35028677+georgkunze@users.noreply.github.com>
2020-04-15 17:05:19 +0200

    Added MITM proxy showcase
    Added MITM proxy showcase and some comments. #8

--- src/test/java/showcase/neodymium/tests/basicauth/BasicAuthenticationTest.java
+++ src/test/java/showcase/neodymium/tests/basicauth/BasicAuthenticationTest.java
@@ -4,6 +4,8 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 
 import com.browserup.bup.proxy.auth.AuthType;
 import com.codeborne.selenide.Selenide;
@@ -28,38 +30,106 @@ import showcase.pageobjects.components.Title;
 @DisplayName("BasicAuthenticationTest")
 
 public class BasicAuthenticationTest extends AbstractTest {
+    
+    @Before
+    public void setup() {
+        //Change property in configuration
+        //Activate use of local proxy
+        Neodymium.configuration().setProperty("neodymium.localproxy", "true");
+        
+        //Teardown webdriver
+        //This is needed in order to reload the configuration and load the changes
+        WebDriverUtils.preventReuseAndTearDown();
+    }
 
-    //@Test
+    @Test
     @Description(value = "Showcase for basic authentication")
     public void testBasicAuthentication() {
+        //Open webdriver with browserprofil of chrome
+        WebDriverUtils.setUp("Chrome_1024x768");
+        
+        //Open webpage with authentication
         Selenide.open("https://authenticationtest.com/HTTPAuth/");
+        
+        //Validate title
         new Title().validateTitle("Authentication Test");
+        
+        //Check that basic alert message is visible
         $(".alert-success").shouldBe(visible);
+        
+        //Close the browser window
         Selenide.closeWindow();
+        
+        //Teardown webdriver
         WebDriverUtils.preventReuseAndTearDown();
     }
     
-    //@Test
+    @Test
     @Description(value = "Showcase for authentication for specific page")
     public void testSpecificPageAuthentication() {
+        //Open webdriver with browserprofil of chrome
         WebDriverUtils.setUp("Chrome_1024x768");
+        
+        //Setup authentication for authenticationtest.com
+        //Note: Syntax here is autoAuthorization(host, username, password, authoraziation_type)
         Neodymium.getLocalProxy().autoAuthorization("authenticationtest.com", "User", "Pass", AuthType.BASIC);
+        
+        //Open authenticationtest.com with http authentication
         Selenide.open("https://authenticationtest.com/HTTPAuth/");
+        
+        //Validate title
         new Title().validateTitle("Authentication Test");
+        
+        //Check that basic alert message is visible
         $(".alert-success").shouldBe(visible);
+        
+        //Close the browser window
         Selenide.closeWindow();
+        
+        //Teardown webdriver
         WebDriverUtils.preventReuseAndTearDown();
     }
     
     @Test
     @Description(value = "Showcase for authentication with MITM proxy")
     public void testMITMProxyAuthentication() {
-        //WebDriverUtils.setUp("Chrome_1024x768");
+        //Change property in configuration
+        //Activate use of specific certificate
+        Neodymium.configuration().setProperty("neodymium.localproxy.certificate", "true");
+        
+        //Open webdriver with browserprofil of chrome
+        WebDriverUtils.setUp("Chrome_1024x768");
+        
+        //Open HTTPS webpage
         Selenide.open("https://docs.mitmproxy.org/stable/");
+        
+        //Validate title
         new Title().validateTitle("Introduction");
+        
+        //Check that mainframe is visible
         $("#main").shouldBe(visible);
+        
+        //Check that sidebar is visible
         $("#sidebar").shouldBe(visible);
+        
+        //Close the browser window
         Selenide.closeWindow();
+        
+        //Teardown webdriver
         WebDriverUtils.preventReuseAndTearDown();
+        
+        //Change property in configuration
+        //Deactivate use of specific certificate
+        Neodymium.configuration().setProperty("neodymium.localproxy.certificate", "false");
+    }    
+    
+    @After
+    public void tearDown() {
+        //Change property in configuration
+        //Deactivate use of local proxy
+        Neodymium.configuration().setProperty("neodymium.localproxy", "false");
+        
+        //Open webdriver with browserprofil of chrome
+        WebDriverUtils.setUp("Chrome_1024x768");
     }
 }
