bd04d81b24c9a033a078240ee7bdded127a06d78
gkunze <gkunze@xceptance.net>
2020-05-08 09:30:10 +0200

    Update PDFDownloadTest.java
    #12
    Added comments

--- src/test/java/showcase/neodymium/tests/pdf/PDFDownloadTest.java
+++ src/test/java/showcase/neodymium/tests/pdf/PDFDownloadTest.java
@@ -32,12 +32,22 @@ public class PDFDownloadTest extends AbstractTest {
     @Test
     @Description(value = "Showcase for PDF testing")
     public void testDownloadPDF() throws IOException, InterruptedException {
+        //Download PDF
         File pdfFile = Selenide.download("https://s1.q4cdn.com/806093406/files/doc_downloads/test.pdf");
+        
+        //wait for download to finish
         sleep(1500);
-        PDDocument document;
-        document = PDDocument.load(pdfFile);
+        
+        //Create new pdf document and load file into it
+        PDDocument document = PDDocument.load(pdfFile);
+        
+        //Read text of PDF into string
         String text = new PDFTextStripper().getText(document);
+        
+        //Check the text
         Assert.assertTrue(text.contains("This is a test PDF document."));
+        
+        //close the dcument
         document.close();
     }
 }
