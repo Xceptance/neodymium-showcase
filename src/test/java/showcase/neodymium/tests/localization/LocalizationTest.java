6e9c469b82b4a477248e267136fe3e43db14de84
gkunze <gkunze@xceptance.net>
2020-06-04 11:42:18 +0200

    Formatting and requested changes
    #9

--- src/test/java/showcase/neodymium/tests/localization/LocalizationTest.java
+++ src/test/java/showcase/neodymium/tests/localization/LocalizationTest.java
@@ -1,10 +1,10 @@
 package showcase.neodymium.tests.localization;
 
-import org.junit.Test;
-
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+import org.junit.Test;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
@@ -26,58 +26,60 @@ import showcase.pageobjects.components.Title;
 @Owner("Georg Kunze")
 @Tag("smoke")
 @DisplayName("Localizationtest")
-public class LocalizationTest extends AbstractTest {
+public class LocalizationTest extends AbstractTest
+{
     @Test
     @Description(value = "Showcase for localization test")
-    public void testLocalization() {
-        //Open homepage in english
-        //Note: To use localized string you have to call them like this:
-        //Neodymium.localizedText(key-to-string)
-        //This will search in the default language as no locale is given over.
+    public void testLocalization()
+    {
+        // Open homepage in english
+        // Note: To use localized string you have to call them like this:
+        // Neodymium.localizedText(key-to-string)
+        // This will search in the default language as no locale is given over.
         Selenide.open(Neodymium.localizedText("homepage.url"));
-        
-        //Check title is correct in english
+
+        // Check title is correct in english
         new Title().validateTitle(Neodymium.localizedText("homepage.title"));
-        
-        //Headline example
-        
-        //Get headline element
+
+        // Headline example
+
+        // Get headline element
         SelenideElement headLine = $("div.landing-intro > h1");
-        
-        //Get subtitle element
+
+        // Get subtitle element
         SelenideElement subTitle = $("div.landing-intro > p");
-        
-        //Check headline element is visible
+
+        // Check headline element is visible
         headLine.shouldBe(visible);
         subTitle.shouldBe(visible);
-        
-        //Check that headline is correct in english
+
+        // Check that headline is correct in english
         headLine.shouldHave(text(Neodymium.localizedText("homepage.headline")));
-        
-        //Close the window
+
+        // Close the window
         Selenide.closeWindow();
-        
-        //Set locale to german(de_DE), so we can check the german version
+
+        // Set locale to german(de_DE), so we can check the german version
         Neodymium.configuration().setProperty("neodymium.locale", "de");
-        
-        //Open homepage in german
+
+        // Open homepage in german
         Selenide.open(Neodymium.localizedText("homepage.url"));
-        
-        //Check title is correct in german
+
+        // Check title is correct in german
         new Title().validateTitle(Neodymium.localizedText("homepage.title"));
-        
-        //Headline example
-        
-        //Check headline element is visible        
+
+        // Headline example
+
+        // Check headline element is visible
         headLine.shouldBe(visible);
         subTitle.shouldBe(visible);
-        
-        //Check that headline is correct in german
+
+        // Check that headline is correct in german
         headLine.shouldHave(text(Neodymium.localizedText("homepage.headline")));
-        
-        //Check that subtitle is correct in german
-        //Note: Here the call to localizedText has a added locale to get a specific String
-        //from a specific locale.
+
+        // Check that subtitle is correct in german
+        // Here the call to localizedText has another locale as second parameter to get
+        // a specific String from that specific locale.
         subTitle.shouldHave(text(Neodymium.localizedText("homepage.subtitle", "german")));
     }
 }
