5d642a752a0d5a8b0468755f231436cb3334e649
Marcel Pfotenhauer <m.pfotenhauer@xceptance.net>
2020-11-05 15:32:14 +0100

    change the test case description to JavaDoc to prevend the issues due to formation try to  make test case description more clear

--- src/test/java/showcase/neodymium/tests/pdf/PdfTextContentCheckTest.java
+++ src/test/java/showcase/neodymium/tests/pdf/PdfTextContentCheckTest.java
@@ -18,36 +18,36 @@ import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.junit4.Tag;
 import showcase.neodymium.tests.AbstractTest;
 
+/**
+ * In some test cases may need to check the content of a PDF file provided by the system under test. Since Neodymium is
+ * implemented in Java and the execution environment is configured with Maven any available 3rd party technology can be
+ * added to the test automation.<br>
+ * This show case gives an basic example how to implement an PDF content check using pdfbox.<br>
+ * <br>
+ * <b>REQUIRED CONFIGURATION</b>
+ * <ul>
+ * <li>pom.xml: - org.apache.pdfbox must be added to the dependencies section</li>
+ * </ul>
+ */
 @Severity(SeverityLevel.NORMAL)
 @Owner("Test Developer")
 @Tag("pdf")
 public class PdfTextContentCheckTest extends AbstractTest
 {
-    /*
-     * In some cases one may need to check at least the content of a PDF file issued by the system under test. Since
-     * Neodymium is implemented in Java based on Maven any available 3rd party technology can be added to the test
-     * automation. This show case gives an basic example how to implement an PDF content check using pdfbox.
-     * 
-     * REQUIRED CONFIGURATION
-     * 
-     * pom.xml: - org.apache.pdfbox must be added to the dependencies section
-     * 
-     */
-
     @Test
     @Description("Showcase for testing PDF text content")
     public void test() throws IOException, InterruptedException, URISyntaxException
     {
-        // Download PDF
+        // download PDF file
         File pdfFile = Selenide.download("https://s1.q4cdn.com/806093406/files/doc_downloads/test.pdf");
 
-        // Create new PDF document and load file into it
+        // create new PDF document and load file into it
         PDDocument document = PDDocument.load(pdfFile);
 
-        // Read text of PDF into string
+        // save the text of the PDF file
         String text = new PDFTextStripper().getText(document);
 
-        // Check the text
+        // check the text
         Assert.assertTrue(text.contains("This is a test PDF document."));
 
         // close the document
