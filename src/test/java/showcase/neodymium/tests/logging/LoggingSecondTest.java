37e5f217a56527e39eb4ccb259d709b26b4ab12f
Uwe Friedel <u.friedel@xceptance.net>
2021-02-22 10:32:05 +0100

    some information in readme file added

--- src/test/java/showcase/neodymium/tests/logging/LoggingSecondTest.java
+++ src/test/java/showcase/neodymium/tests/logging/LoggingSecondTest.java
@@ -10,12 +10,6 @@ public class LoggingSecondTest
 {
     private static Logger logger = LogManager.getLogger(LoggingSecondTest.class);
 
-    // log4j-core is included in the neodymium-library
-    // it is not necessary to add an entry in the pom
-    // ConfigurationFactory will look for log4j2-test.properties in the classpath
-    // then it will look for log4j2.properties on the classpath
-    // the same goes for yaml, json and xml files
-    // as soon as the search was successful, log4j will stop and load the configuration file
     @Test
     @Description("Showcase for logging with log4j.")
     public void testSecondLogging()
