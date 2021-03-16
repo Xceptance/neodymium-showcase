6d038ce7c41be2e14b17b7eb55cf6794125ee385
Uwe Friedel <u.friedel@xceptance.net>
2021-03-16 18:57:14 +0100

    same style implemented - xpath replaced

--- src/test/java/showcase/neodymium/tests/iframe/SelenideSwitchToFrameTest.java
+++ src/test/java/showcase/neodymium/tests/iframe/SelenideSwitchToFrameTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 
 import com.codeborne.selenide.Selenide;
-import com.xceptance.neodymium.util.AllureAddons;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.junit4.Tag;
@@ -17,44 +16,36 @@ import showcase.neodymium.tests.AbstractTest;
 /**
  * This show case explains with an example implementation how to test a web site with frames (iframe).<br>
  * Selenide offers a straight forward approach to handle such frames and within Neodymium this is simply applied.<br>
- * <b>REQUIRED CONFIGURATION</b>
- * <i>config/dev-neodymium.properties</i>:
+ * <b>REQUIRED CONFIGURATION</b> <i>config/dev-neodymium.properties</i>:
  * <ul>
- * <li>neodymium.webDriver.chrome.pathToDriverServer = <local path to chrome webdriver executable> # webdriver configuration</li>
+ * <li>neodymium.webDriver.chrome.pathToDriverServer = <local path to chrome webdriver executable> # webdriver
+ * configuration</li>
  * <li>neodymium.debugUtils.highlight = true # suggestion</li>
  * </ul>
- * <b>RUN</b>
- * To run this test case and see the report try this from within a command line
- * $ mvn -Dtest=SelenideSwitchToFrameTest clean test allure:serve
+ * <b>RUN</b> To run this test case and see the report try this from within a command line $ mvn
+ * -Dtest=SelenideSwitchToFrameTest clean test allure:serve
  */
 @Tag("iFrame")
-public class SelenideSwitchToFrameTest extends AbstractTest {
-  @Test
-  @Description(value = "Showcase for iFrames.")
-  public void test() {
-    AllureAddons.step("Open Site with Frame", () -> {
-      // open a page known to hold an iframe
-      Selenide.open("https://www.w3schools.com/html/html_iframe.asp");
-    });
+public class SelenideSwitchToFrameTest extends AbstractTest
+{
+    @Test
+    @Description(value = "Showcase for iFrames.")
+    public void test()
+    {
+        // open a page known to hold an iframe
+        Selenide.open("https://www.w3schools.com/html/html_iframe.asp");
 
-    AllureAddons.step("Check Frame Heading and Main Page Heading", () -> {
-      AllureAddons.step("Check Frame Heading", () -> {
         // before executing any check within an iframe switch into it
         // the switch can be performed via id, name, index or WebElement like this
-        switchTo().frame($(By.xpath("//iframe[@title='W3Schools HTML Tutorial']")));
+        switchTo().frame($(By.cssSelector("iframe[src=\"default.asp\"]")));
 
         // do a check within the frame
-        $(By.xpath("//h1")).should(matchText("HTML Tutorial"));
-      });
-      
-      AllureAddons.step("Check Main Page Heading", () -> {
+        $(".w3-col.l10.m12#main > h1").should(matchText("HTML Tutorial"));
+
         // switch back to the page
         switchTo().defaultContent();
 
         // do the same check as within the frame again, but expect another heading text
-        $(By.xpath("//h1")).should(matchText("HTML Iframes"));
-      });
-    });
-  }
-
+        $(".w3-col.l10.m12#main > h1").should(matchText("HTML Iframes"));
+    }
 }
