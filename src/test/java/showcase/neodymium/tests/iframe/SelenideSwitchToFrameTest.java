a5aef0d4b244f4e7b84c1da9d66c39e6e965d91b
Uwe Friedel <u.friedel@xceptance.net>
2021-03-22 17:50:54 +0100

    comments updated

--- src/test/java/showcase/neodymium/tests/iframe/SelenideSwitchToFrameTest.java
+++ src/test/java/showcase/neodymium/tests/iframe/SelenideSwitchToFrameTest.java
@@ -5,25 +5,17 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.switchTo;
 
 import org.junit.Test;
-import org.openqa.selenium.By;
 
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.junit4.Tag;
 import showcase.neodymium.tests.AbstractTest;
 
 /**
- * This show case explains with an example implementation how to test a web site with frames (iframe).<br>
+ * This show case explains with an example implementation how to test a web site using frames (iframe).<br>
  * Selenide offers a straight forward approach to handle such frames and within Neodymium this is simply applied.<br>
- * <b>REQUIRED CONFIGURATION</b> <i>config/dev-neodymium.properties</i>:
- * <ul>
- * <li>neodymium.webDriver.chrome.pathToDriverServer = <local path to chrome webdriver executable> # webdriver
- * configuration</li>
- * <li>neodymium.debugUtils.highlight = true # suggestion</li>
- * </ul>
- * <b>RUN</b> To run this test case and see the report try this from within a command line $ mvn
- * -Dtest=SelenideSwitchToFrameTest clean test allure:serve
  */
 @Tag("iFrame")
 public class SelenideSwitchToFrameTest extends AbstractTest
@@ -37,15 +29,16 @@ public class SelenideSwitchToFrameTest extends AbstractTest
 
         // before executing any check within an iframe switch into it
         // the switch can be performed via id, name, index or WebElement like this
-        switchTo().frame($(By.cssSelector("iframe[src=\"default.asp\"]")));
+        SelenideElement iFrame = $("iframe[src='default.asp']");
+        switchTo().frame(iFrame);
 
-        // do a check within the frame
+        // do a check for heading text within the frame
         $(".w3-col.l10.m12#main > h1").should(matchText("HTML Tutorial"));
 
         // switch back to the page
         switchTo().defaultContent();
 
-        // do the same check as within the frame again, but expect another heading text
+        // do the same check as within the frame again, but expect to find the heading text of the main page
         $(".w3-col.l10.m12#main > h1").should(matchText("HTML Iframes"));
     }
 }
